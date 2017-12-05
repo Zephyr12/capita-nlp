b22d95d738239227f552961413585483e7bb0c9f
Amartya Vadlamani <amartya.vadlamani@gmail.com>
2017-12-05 15:55:37 +0000

    added basic data processing infrastructure to the project

---
+++ src/pipes.py
@@ -0,0 +1,49 @@
+import threading
+
+Message = dict
+
+class Node:
+    def __init__(self, operation):
+        self.out_pipes = []
+        self.operation = operation
+    
+    def add_out_pipe(self, pipe):
+        self.out_pipes.append(pipe)
+
+    def process(self, msg):
+        results = self.operation(msg)
+        for pipe in self.out_pipes:
+            for result in results:
+                pipe.process(result)
+
+class Source(Node):
+    
+    def run(self):
+        t = threading.Thread(target=lambda: self.process(None))
+        t.daemon = False
+        t.start()
+        return t
+
+class Sink(Node):
+    pass
+
+class Merge(Node):
+    
+    def __init__(self, join_key, is_complete):
+        super().__init__(lambda x: x)
+        self.join_key = join_key
+        self.is_complete = is_complete
+        self.cached = {}
+
+    def process(self, msg):
+        key = self.join_key(msg)
+        if key in self.cached:
+            self.cached[key].update(msg)
+            if self.is_complete(self.cached[key]):
+                super().process([self.cached[key]])
+        else:
+            self.cached[key] = msg
+
+
+class Processor(Node):
+    pass
