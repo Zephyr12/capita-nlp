3101b3016f9065c6cccf42fd181861dc75324a3c
Amartya Vadlamani <amartya.vadlamani@gmail.com>
2018-01-23 19:19:53 +0000

    fixed pipelining

--- src/pipes.py
+++ src/pipes.py
@@ -2,50 +2,47 @@ import threading
 
 Message = dict
 
-class Node:
-    def __init__(self, operation):
-        self.out_pipes = []
-        self.operation = operation
+class Source:
+
+    def __init__(self, sinks, source):
+        self.source = source
+        self.processors = sinks
+        thread = threading.Thread(target=lambda: self.start())
+        thread.start()
     
-    def add_out_pipe(self, pipe):
-        self.out_pipes.append(pipe)
+    def start(self):
+        for item in self.source():
+            for processor in self.processors:
+                processor.queue(item)
 
-    def process(self, msg):
-        results = self.operation(msg)
-        for result in results:
-            for pipe in self.out_pipes:
-                pipe.process(result)
+class Processor:
 
-class Source(Node):
-    
-    def run(self):
-        t = threading.Thread(target=lambda: self.process(None))
-        t.daemon = False
-        t.start()
-        return t
+    def __init__(self, sinks, process):
+        self.process = process
+        self.todo = []
+        self.sinks = sinks
 
-class Sink(Node):
-    pass
-
-class Merge(Node):
-    
-    def __init__(self, join_key, is_complete):
-        super().__init__(lambda x: x)
-        self.join_key = join_key
-        self.is_complete = is_complete
-        self.cached = {}
-
-    def process(self, msg):
-        key = self.join_key(msg)
-        if key in self.cached:
-            self.cached[key].update(msg)
-            if self.is_complete(self.cached[key]):
-                cached = self.cached[key]
-                del self.cached[key]
-                super().process([cached])
-        else:
-            self.cached[key] = msg
+    def dispatch(self, item):
+        for output in self.process(item):
+            for sink in self.sinks:
+                sink.queue(output)
 
 
-class Processor(Node):
-    pass
+    def queue(self, item):
+        threading.Thread(target=lambda: self.dispatch(item)).start()
+
+class Writer:
+
+    def __init__(self, writer):
+        self.writer = writer
+
+    def queue(self, item):
+        threading.Thread(target=lambda: self.writer(item)).start()
+
+if __name__ == "__main__":
+    src = Source([
+                Processor(
+                        [Writer(lambda x: print("WRITTEN", x))],
+                        lambda x: [x * x])],
+                lambda: range(100000)
+            )
