c6d06927e3ed3f6a02192912e136b5d9d806ace3
bogdann1 <zcabnit@ucl.ac.uk>
2018-02-19 19:36:45 +0000

    Stores all events since current date in 'today-date.csv' file.

--- src/fb.py
+++ src/fb.py
@@ -2,19 +2,24 @@ import facebook
 import json
 import requests
 import pandas
+import csv
+import datetime
+import os
 
-my_token = 'your token'
+
+my_token = 'EAACEdEose0cBADtRyv4Vq5IoA0buSOpmKj2TozAADMzKvzlNija2T4UOTKWNkBI30TeQuPBS5knzZA4jJCEAIOWWoRwLuAzSwtDxkUsZAqBZCluTP6lXqeOfpM7m7QLesQU19g2ZB0AW3UVNvg9x3yZAOuWW8vCJdXlN4awMWRHJgrZBZBM7UQmhxpUtPxCoIU0Tfv2bDKlVO5Hm5aWc7nG'
 BASE_URL = 'https://graph.facebook.com/'
 counter = 0
 
 graph = facebook.GraphAPI(my_token)
 
-schools = pandas.read_csv("data/EduBase_Schools_UTF-8.csv").school_name.tolist()
+#schools = pandas.read_csv("EduBase_Schools_UTF-8.csv").school_name.tolist()
+schools = ['UCL']
 
 
-def get_data(school):
+def get_data(school,since):
     global data, events, next_page
-    data = graph.request('search', {'q': school, 'type': 'event'})
+    data = graph.request('search', {'q': school, 'type': 'event', 'since':since})
     events = json.dumps(data, indent=4, sort_keys=True)
     try:
         next_page = data["paging"]["next"]
@@ -25,20 +30,34 @@ def get_data(school):
 #get_data()
 
 
-def print_event_name_and_description():
+def print_event_name_and_description(since):
     global counter
     for my_data in data["data"]:
-        counter += 1
         try:
-            name = my_data['name']
-            description = my_data['description']
+            country = my_data['place']['location']['country']
         except KeyError:
-            break
-        try:
-            location = my_data['place']['location']['city']
-        except KeyError:
-            location = "No location displayed"
-        print(str(counter) + ": " + name + " \n " + "Location: " + location + "\n" + description + "\n\n" + "----------------------------------------------------" + "\n")
+            country = "Country not displayed"
+        if country == "United Kingdom" or country == "Country not displayed":
+            counter += 1
+            try:
+                name = my_data['name']
+                description = my_data['description']
+            except KeyError:
+                break
+            try:
+                location = my_data['place']['location']['city']
+            except KeyError:
+                location = "No location displayed"
+            print(str(counter) + ": " + name + " \n " + "Location: " + location + "\n" + description + "\n\n" + "----------------------------------------------------" + "\n")
+            fields = [name, location, description]
+            file_name = since + ".csv"
+            with open(file_name, 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(fields)
+                f.close()
+
+
+
 
 def print_pretty_events(events):
     print (json.dumps(events,indent=4,sort_keys=True))
@@ -51,11 +70,11 @@ def print_pretty_events(events):
 #events = data
 
 
-def iterate_throught_pages():
+def iterate_throught_pages(since):
     global data, events, next_page
     while next_page:
         data = events
-        print_event_name_and_description()
+        print_event_name_and_description(since)
         #print_pretty_events(data)
         events = json.loads(requests.get(next_page).text)
         try:
@@ -66,7 +85,16 @@ def iterate_throught_pages():
 
 #iterate_throught_pages()
 
+
+
+
 for school in schools:
-    get_data(school)
-    events = data
-    iterate_throught_pages()
+    now = datetime.datetime.now()
+    since = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+    try:
+        with open(since+'.csv') as file:
+            print("Queries up-to-date.")
+    except IOError as e:
+        get_data(school, since)
+        events = data
+        iterate_throught_pages(since)
