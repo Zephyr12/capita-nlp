91d6d2eabb698eb57c816979d741c1b22f928306
bogdann1 <zcabnit@ucl.ac.uk>
2018-03-12 17:42:07 +0000

    committing the refactored facebook source

--- src/refactoredFacebookSource.py
+++ src/refactoredFacebookSource.py
@@ -4,6 +4,8 @@ import requests
 import pandas
 import csv
 import datetime
+import hashlib
+import base64
 
 
 
@@ -11,21 +13,27 @@ import datetime
 
 class FacebookData:
     BASE_URL = 'https://graph.facebook.com/'
-    facebook_token = 'EAACEdEose0cBADWdYqmZCOkZAyr0zk0Bz689mr7XIkfqiSNYhtaiD6PJrDzLMV2zHBk3WhoiTfFQhXPlAp8KaPgENL4fFQvxqpunPsyUkkFOth9EhAb4bwk2kbswrVKuZBkVLwv10VmH7xYfzZAKaeUefnR57HZAxT7PbstzsZC1Je0FfIkf5VHL68KrkIvnM5NI3peKG5U3E3JtSZBMwDH'
+    facebook_token = 'EAACEdEose0cBANo0IKaDsnfVJ1mTHjSfrGakdQpyBAj0LM3vmVjyVucKNKtj0kSc6LSpAIgdNgDZBVjlJNFT5tnqhZADxwiLIsJIbpIbNTiVcsg3Hd4sjl1MCUz4ZCOcMuDZAsqR6iNBfPJyUwkmyu9cDJ7KQ1zDStjFV0Sikrh4qxXeD02TB6aAVqMDum793soBSSykrAKPRcZBwvsZBT'
 
     graph = facebook.GraphAPI(facebook_token)
 
     counter = 0
 
     # schools = pandas.read_csv("EduBase_Schools_UTF-8.csv").school_name.tolist()
-    schools = ['UCL','University College London', 'Kings College London','Imperial College London']
 
-    def __init__(self):
+    def __init__(self, schools=['UCL', 'University College London', 'Kings College London', 'Imperial College London']):
         self.data = None
         self.events = None
         self.next_page = None
+        self.schools = schools
 
     def get_school_events(self, school, since):
+        """
+        Gets school events
+        :param school: the school
+        :param since:
+        :return:
+        """
         self.data = self.graph.request('search', {'q': school, 'type': 'event', 'since': since})
         self.events = json.dumps(self.data, indent=4, sort_keys=True)
         try:
@@ -34,7 +42,7 @@ class FacebookData:
             self.next_page = None
 
 
-    def get_event_details(self, since):
+    def get_event_details(self, since, school):
         for my_data in self.data["data"]:
             country = self.get_event_country(my_data)
             if country == "United Kingdom":  # or country == "Country not displayed":
@@ -46,15 +54,15 @@ class FacebookData:
                     break
                 location = self.get_event_location(my_data)
                 # print(str(counter) + ": " + name + " \n " + "Location: " + location + "\n" + description + "\n\n" + "----------------------------------------------------" + "\n")
-                self.add_event_details_to_cache_file(description, location, name, since)
+                self.add_event_details_to_cache_file(description, location, name, since, school)
                 yield from self.yield_event_details(description, location, name)
 
     def yield_event_details(self, description, location, name):
         yield (str(
             self.counter) + ": " + name + " \n " + "Location: " + location + "\n" + description + "\n\n" + "----------------------------------------------------" + "\n")
 
-    def add_event_details_to_cache_file(self, description, location, name, since):
-        fields = [name, location, description]
+    def add_event_details_to_cache_file(self, description, location, name, since, school):
+        fields = [school, name, location, description]
         with open('data/' + self.get_cache_ID(since), 'a') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
@@ -78,10 +86,10 @@ class FacebookData:
         print(json.dumps(events, indent=4, sort_keys=True))
 
 
-    def iterate_throught_pages(self, since):
+    def iterate_throught_pages(self, since, school):
         while self.next_page:
             self.data = self.events
-            for i in self.get_event_details(since):
+            for i in self.get_event_details(since, school):
                 yield i
             # print_pretty_events(data)
             self.events = json.loads(requests.get(self.next_page).text)
@@ -100,14 +108,14 @@ class FacebookData:
             for school in self.schools:
                 self.get_school_events(school, since)
                 self.events = self.data
-                for i in self.iterate_throught_pages(since):
+                for i in self.iterate_throught_pages(since, school):
                     # print(i)
                     yield i
 
     def create_cache_header(self, since):
         file_name = self.get_cache_ID(since)
         with open('data/' + file_name, 'a') as f:
-            writer = csv.DictWriter(f, fieldnames=["name", "location", "raw_text"])
+            writer = csv.DictWriter(f, fieldnames=["query", "name", "location", "raw_text"])
             writer.writeheader()
 
     def get_cache_ID(self, since):
@@ -116,7 +124,7 @@ class FacebookData:
             file_name += "-"
             file_name += str(school)
         file_name += ".csv"
-        return file_name
+        return hashlib.sha1(bytes(file_name, encoding='UTF-8')).hexdigest()
 
     def yield_rows_from_cache_file(self, since):
         with open('data/' + self.get_cache_ID(since)) as f:
@@ -130,7 +138,7 @@ class FacebookData:
         return since
 
 
-x = FacebookData()
+x = FacebookData(list(map(lambda r: r["EstablishmentName"], filter(lambda e: e["Town"] == "London", csv.DictReader(open("data/schools.csv"))))))
 for i in x.run():
     print(i)
 
